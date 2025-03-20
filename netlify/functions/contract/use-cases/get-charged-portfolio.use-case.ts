23ce4310467130476aeb8600109e5418cd6c5e5a
JKmoises <moi.prado@duocuc.cl>
2025-03-19 22:27:16 -0300

    feat: Agregar totales a query SQL de caso de uso para obtener cartera cobrada

--- netlify/functions/contract/use-cases/get-charged-portfolio.use-case.ts
+++ netlify/functions/contract/use-cases/get-charged-portfolio.use-case.ts
@@ -1,7 +1,12 @@
 import { HandlerResponse } from "@netlify/functions";
 
 import { db } from "../../../data/db";
-import { chargesTable, contractsTable, customersTable, financingTable } from "../../../data/schemas";
+import {
+  chargesTable,
+  contractsTable,
+  customersTable,
+  financingTable,
+} from "../../../data/schemas";
 
 import { HEADERS } from "../../../config/constants";
 
@@ -58,58 +63,81 @@ export class GetChargedPortfolio implements GetChargedPortfolioUseCase {
     try {
       const chargedPortfolioContracts = await db.execute(sql`
         SELECT 
-        c.id AS contrato,
-        c.asesor_credito  AS oficial_credito,
-        c.ubicacion AS ubicacion,
-        l.nombre AS cliente,
-        (SELECT MAX(fecha_vencimiento) FROM ${financingTable} WHERE id_contrato = c.id) AS fecha_entrega,
-        COALESCE((SELECT valor_dividendos FROM ${financingTable} 
-          WHERE  tipo_dividendo = 'Cuota Inicial' AND id_contrato = c.id), 0) AS cuota_inicial,
+        proyecto,
+        contrato,
+        oficial_credito,
+        ubicacion,
+        cliente,
+        fecha_entrega,
+        cuota_inicial,
+        vencida_menor_30_fb,
+        vencida_mayor_30_fb,
+        al_tiemopo_fb,
+        prepago_fb,
+        (vencida_menor_30_fb + vencida_mayor_30_fb + al_tiemopo_fb + prepago_fb) as total_cobradofb,
+        vencida_menor_30_ce,
+        vencida_mayor_30_ce,
+        al_tiemopo_ce,
+        prepago_ce,
+        (vencida_menor_30_ce + vencida_mayor_30_ce + al_tiemopo_ce + prepago_ce) as total_cobradoce,
+        (vencida_menor_30_fb + vencida_mayor_30_fb + al_tiemopo_fb + prepago_fb + vencida_menor_30_ce 
+        + vencida_mayor_30_ce + al_tiemopo_ce + prepago_ce) as total
+
+
+        FROM
+        (select 
+        c.proyecto,
+        c.id as contrato,
+        c.asesor_credito  as oficial_credito,
+        c.ubicacion as ubicacion,
+        l.nombre as cliente,
+        (select max(fecha_vencimiento) FROM ${financingTable} WHERE id_contrato = c.id) as fecha_entrega,
+        COALESCE((select valor_dividendos FROM ${financingTable} 
+          WHERE  tipo_dividendo = 'Cuota Inicial' AND id_contrato = c.id), 0) as cuota_inicial,
           
-        COALESCE((SELECT SUM(case WHEN r.fecha_cobro <= DATE_ADD(f.fecha_vencimiento, '30 days') AND 
-        r.fecha_cobro >= f.fecha_vencimiento THEN r.valor_cobrado ELSE 0 end) AS valor
-          FROM ${chargesTable} AS r WHERE  r.id_contrato = c.id AND r.tipo_dividendo = 'Financiamiento Bancario' AND 
-          r.num_dividendo = f.numero_dividendo), 0) AS vencida_menor_30_fb,
+        COALESCE((select sum(case WHEN r.fecha_cobro <= date_add(f.fecha_vencimiento, '30 days') AND 
+          r.fecha_cobro >= f.fecha_vencimiento THEN r.valor_cobrado ELSE 0 end) as valor
+          FROM ${chargesTable} as r WHERE  r.id_contrato = c.id AND r.tipo_dividendo = 'Financiamiento Bancario' AND 
+          r.num_dividendo = f.numero_dividendo), 0) as vencida_menor_30_fb,
 
-        COALESCE((SELECT SUM(case WHEN s.fecha_cobro >  DATE_ADD(f.fecha_vencimiento, '30 days') THEN s.valor_cobrado ELSE 0 end) AS valor
-          FROM ${chargesTable} AS s WHERE  s.id_contrato = c.id AND s.tipo_dividendo = 'Financiamiento Bancario' AND 
-          s.num_dividendo = f.numero_dividendo), 0) AS vencida_mayor_30_fb,
+        COALESCE((select sum(case WHEN s.fecha_cobro >  date_add(f.fecha_vencimiento, '30 days') THEN s.valor_cobrado ELSE 0 end) as valor
+          FROM ${chargesTable} as s WHERE  s.id_contrato = c.id AND s.tipo_dividendo = 'Financiamiento Bancario' AND 
+          s.num_dividendo = f.numero_dividendo), 0) as vencida_mayor_30_fb,
           
-        COALESCE((SELECT SUM(case WHEN s.fecha_cobro = f.fecha_vencimiento THEN s.valor_cobrado ELSE 0 end) AS valor
-          FROM ${chargesTable} AS s WHERE  s.id_contrato = c.id AND s.tipo_dividendo = 'Financiamiento Bancario' AND 
-          s.num_dividendo = f.numero_dividendo), 0) AS al_tiemopo_fb,
+        COALESCE((select sum(case WHEN s.fecha_cobro = f.fecha_vencimiento THEN s.valor_cobrado ELSE 0 end) as valor
+          FROM ${chargesTable} as s WHERE  s.id_contrato = c.id AND s.tipo_dividendo = 'Financiamiento Bancario' AND 
+          s.num_dividendo = f.numero_dividendo), 0) as al_tiemopo_fb,
 
-        COALESCE((SELECT SUM(case WHEN t.fecha_cobro < f.fecha_vencimiento THEN t.valor_cobrado ELSE 0 end) AS valor
-          FROM ${chargesTable} AS t WHERE  t.id_contrato = c.id AND t.tipo_dividendo = 'Financiamiento Bancario' AND 
-          t.num_dividendo = f.numero_dividendo), 0) AS prepago_fb,
+        COALESCE((select sum(case WHEN t.fecha_cobro < f.fecha_vencimiento THEN t.valor_cobrado ELSE 0 end) as valor
+          FROM ${chargesTable} as t WHERE  t.id_contrato = c.id AND t.tipo_dividendo = 'Financiamiento Bancario' AND 
+          t.num_dividendo = f.numero_dividendo), 0) as prepago_fb,
 
-        COALESCE((SELECT SUM(case WHEN r.fecha_cobro <= DATE_ADD(f.fecha_vencimiento, '30 days') AND 
-        r.fecha_cobro >= f.fecha_vencimiento THEN r.valor_cobrado ELSE 0 end) AS valor
-          FROM ${chargesTable} AS r WHERE  r.id_contrato = c.id AND r.tipo_dividendo = 'Cuota de Entrada' AND 
-          r.num_dividendo = f.numero_dividendo), 0) AS vencida_menor_30_ce,
+        COALESCE((select sum(case WHEN r.fecha_cobro <= date_add(f.fecha_vencimiento, '30 days') AND 
+        r.fecha_cobro >= f.fecha_vencimiento THEN r.valor_cobrado ELSE 0 end) as valor
+          FROM ${chargesTable} as r WHERE  r.id_contrato = c.id AND r.tipo_dividendo = 'Cuota de Entrada' AND 
+          r.num_dividendo = f.numero_dividendo), 0) as vencida_menor_30_ce,
 
-        COALESCE((SELECT SUM(case WHEN s.fecha_cobro >  DATE_ADD(f.fecha_vencimiento, '30 days') THEN s.valor_cobrado ELSE 0 end) AS valor
-          FROM ${chargesTable} AS s WHERE  s.id_contrato = c.id AND s.tipo_dividendo = 'Cuota de Entrada' AND 
-          s.num_dividendo = f.numero_dividendo), 0) AS vencida_mayor_30_ce,
+        COALESCE((select sum(case WHEN s.fecha_cobro >  date_add(f.fecha_vencimiento, '30 days') THEN s.valor_cobrado ELSE 0 end) as valor
+          FROM ${chargesTable} as s WHERE  s.id_contrato = c.id AND s.tipo_dividendo = 'Cuota de Entrada' AND 
+          s.num_dividendo = f.numero_dividendo), 0) as vencida_mayor_30_ce,
           
-        COALESCE((SELECT SUM(case WHEN s.fecha_cobro = f.fecha_vencimiento THEN s.valor_cobrado ELSE 0 end) AS valor
-          FROM ${chargesTable} AS s WHERE  s.id_contrato = c.id AND s.tipo_dividendo = 'Cuota de Entrada' AND 
-          s.num_dividendo = f.numero_dividendo), 0) AS al_tiemopo_ce,
+        COALESCE((select sum(case WHEN s.fecha_cobro = f.fecha_vencimiento THEN s.valor_cobrado ELSE 0 end) as valor
+          FROM ${chargesTable} as s WHERE  s.id_contrato = c.id AND s.tipo_dividendo = 'Cuota de Entrada' AND 
+          s.num_dividendo = f.numero_dividendo), 0) as al_tiemopo_ce,
 
-        COALESCE((SELECT SUM(case WHEN t.fecha_cobro < f.fecha_vencimiento THEN t.valor_cobrado ELSE 0 end) AS valor
-          FROM ${chargesTable} AS t WHERE  t.id_contrato = c.id AND t.tipo_dividendo = 'Cuota de Entrada' AND 
-          t.num_dividendo = f.numero_dividendo), 0) AS prepago_ce
+        COALESCE((select sum(case WHEN t.fecha_cobro < f.fecha_vencimiento THEN t.valor_cobrado ELSE 0 end) as valor
+          FROM ${chargesTable} as t WHERE  t.id_contrato = c.id AND t.tipo_dividendo = 'Cuota de Entrada' AND 
+          t.num_dividendo = f.numero_dividendo), 0) as prepago_ce
 
-          FROM ${contractsTable}  AS c
-          JOIN ${financingTable} AS f ON  f.id_contrato = c.id 
-          JOIN ${customersTable} AS l ON l.id = c.cliente_id
-          WHERE f.estado_dividendo = 'Pagada' AND f.fecha_vencimiento BETWEEN ${rangeStartDate} AND ${rangeEndDate}
-
-          GROUP by c.id, l.nombre, vencida_menor_30_fb, vencida_mayor_30_fb, al_tiemopo_fb, prepago_fb, vencida_menor_30_ce, 
-                    vencida_mayor_30_ce, al_tiemopo_ce, prepago_ce
-          ORDER by c.id ASC
+        FROM ${contractsTable}  as c
+        JOIN ${financingTable} as f ON  f.id_contrato = c.id 
+        JOIN ${customersTable} as l ON l.id = c.cliente_id
+        WHERE f.estado_dividendo = 'Pagada' AND f.fecha_vencimiento BETWEEN '2024-01-01' AND '2024-01-31'
+              AND c.proyecto = 'NUMA'
+        GROUP BY c.id, l.nombre, vencida_menor_30_fb, vencida_mayor_30_fb, al_tiemopo_fb, prepago_fb, vencida_menor_30_ce, 
+                  vencida_mayor_30_ce, al_tiemopo_ce, prepago_ce
+        ORDER BY c.id)
       `);
-
 
       return {
         statusCode: 200,
