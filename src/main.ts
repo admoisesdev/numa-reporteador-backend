8a3139cf093c3da20adab5a6ab4845c5ec2cb839
JKmoises <moi.prado@duocuc.cl>
2025-04-19 18:49:49 -0400

    feat: Migrar netlify functions a API REST con NestJS

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: [
+      process.env.FRONTEND_URL,
+      'http://localhost:5173',
+      '34.71.107.222',
+    ],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Numa API Rest')
+    .setDescription('Numa endpoints reportes, clientes, contratos, etc...')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(3000);
+  logger.log(`App running on port ${process.env.PORT}`);
+}
+bootstrap();
